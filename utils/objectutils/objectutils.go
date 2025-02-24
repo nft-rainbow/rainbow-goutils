49a915238ffe52b431746c61bb0672cb986a9578
dayong <wangdayong228@163.com>
2025-02-24 16:04:48 +0800

    update AllFieldsNotEmpty/AllFieldsEmpty

--- utils/objectutils/objectutils.go
+++ utils/objectutils/objectutils.go
@@ -18,8 +18,29 @@ func AllFieldsNotEmpty(obj interface{}) bool {
 	for i := 0; i < v.NumField(); i++ {
 		field := v.Field(i)
 
-		// 检查字段类型是否为字符串
-		if field.Kind() == reflect.String && field.String() == "" {
+		// 检查字段类型是否为零值
+		if field.IsZero() {
+			return false
+		}
+	}
+	return true
+}
+
+func AllFieldsEmpty(obj interface{}) bool {
+	// 获取对象的值
+	v := reflect.ValueOf(obj)
+
+	// 确保传入的是结构体类型
+	if v.Kind() != reflect.Struct {
+		return false
+	}
+
+	// 遍历结构体的所有字段
+	for i := 0; i < v.NumField(); i++ {
+		field := v.Field(i)
+
+		// 检查字段类型是否非零值
+		if !field.IsZero() {
 			return false
 		}
 	}
